1bd9aaeed31b28b6c79f58a0014f42a18eb05cf4
masmaspedo <pentingmain@gmail.com>
2021-04-29 16:10:45 +0800

    Challange done

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+function fizzbuz(total) {
+    for (var i = 1; i <= total; i++){
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            console.log(i,' : Fizz Buzz')
+        } else if (i % 3 == 0) {
+            console.log(i, ' : Fizz')
+        } else if (i % 5 == 0){
+            console.log(i, ' : Buzz')
+        } else {
+            console.log(i)
+        }
+    }
+}
+
+fizzbuz(50)
